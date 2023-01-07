2d8932320634e4dd2ae014b411faa360731e8f81
”sovsianykov” <swebproger@gmail.com>
2023-01-07 09:49:45 +0200

    mongoose fixed 2

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 
 const mongoDB = async () =>{
-  const conn  =  await   mongoose.connect(process.env.MONGO_URL)
+  const conn  =  await   mongoose.connect('mongodb+srv://serhii:1111@cluster0.gjab3dl.mongodb.net/products?retryWrites=true&w=majority')
   await  console.log(`Mongo DB is running: ${conn.connection.host}`)
 }
 
